412c0eaf20baeb8486cd9879af58bfd642dab043
kduprie <41270087+kduprie@users.noreply.github.com>
2024-05-01 20:29:00 +0000

    day3.4

--- demos/tools_api/schemas.py
+++ demos/tools_api/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import re
 
 class BaseCar(BaseModel):
     make: str
@@ -6,6 +7,20 @@ class BaseCar(BaseModel):
     year: int
     color: str
     price: float
+
+    @field_validator("year")
+    @classmethod
+    def validate_year(cls, value: int) -> int:
+        if value < 1886:
+            raise ValueError("impossible year")
+        return value
+
+    @field_validator("price")
+    @classmethod
+    def validate_price(cls, value: float) -> float:
+        if value < 0.0:
+            raise ValueError("price must be positive")
+        return value
 
 class CarCreate(BaseCar):
     ...
@@ -19,6 +34,20 @@ class Car(BaseCar):
 class BaseColor(BaseModel):
     name: str
     hex_code: str
+
+    @field_validator("name", "hex_code")
+    @classmethod
+    def validate_required(cls, value: str) -> str:
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("is required")
+        return value
+
+    @field_validator("hex_code")
+    @classmethod
+    def validate_hex_code(cls, value: str) -> str:
+        if not re.match(r"^#(?:[0-9a-fA-F]{3}){1,2}$", value):
+            raise ValueError("not a valid CSS hex code")
+        return value.lower()
 
 class ColorCreate(BaseColor):
     ...
